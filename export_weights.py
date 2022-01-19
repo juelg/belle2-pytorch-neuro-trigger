56b6d231e2896b4559fd497eef9924f89c1502cc
Tobias Jülg <tobias.juelg@tum.de>
2022-01-19 11:42:36 +0100

    Added export weight script

---
+++ export_weights.py
@@ -0,0 +1,47 @@
+# %%
+# Utility script that exports weights from trained net checkpoints to a json file
+from typing import OrderedDict
+import os
+import torch
+import json
+
+
+def create_json_for_conf(conf):
+    experts = [f"expert_{i}" for i in range(5)]
+    exptert_paths = [os.path.join("log", conf, "version_0", expert, "ckpts") for expert in experts]
+
+    fn = []
+    for expert in exptert_paths:
+        fn.append([i for i in os.listdir(expert) if i.startswith("epoch")][0])
+
+    exps = OrderedDict()
+    for expert, path, fn in zip(experts, exptert_paths, fn):
+
+        # new_model = pl_module.NeuroTrigger.load_from_checkpoint(checkpoint_path=exptert_paths[0])
+        checkpoint = torch.load(os.path.join(path, fn))
+
+
+        # model = model.BaselineModel()
+        # model.load_state_dict(checkpoint["state_dict"])
+        desc = {}
+        for key, value in checkpoint["state_dict"].items():
+            print(value.shape)
+            desc[key] = value.shape
+            checkpoint["state_dict"][key] = value.tolist()
+        # print(checkpoint["state_dict"])
+        exps[expert] = {"shapes": desc, "weights": checkpoint["state_dict"]}
+
+        with open(os.path.join(path, "wights.json"), "w") as f:
+            json.dump(exps[expert], f)
+
+    with open(os.path.join("log", conf, "version_0", "wights.json"), "w") as f:
+        json.dump(exps, f)
+
+
+conf = "baseline_v1"
+create_json_for_conf(conf)
+
+
+
+
+# %%
