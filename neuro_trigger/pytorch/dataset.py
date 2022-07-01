dataset weighting tests and fixes

neuro_trigger/pytorch/dataset.py
@@ -52,6 +52,7 @@ class DatasetPath:
 
 
 
+# TODO: support for merging several files
 # TODO: should this also manage dataset versions and dataset splits -> yes splits would definatly make sense
 # TODO: add preprocessing, maybe pytorch transformers
 class BelleIIDataManager:
@@ -157,6 +158,8 @@ class BelleIIDataset(Dataset):
         return len(self.data["x"])
 
     def __getitem__(self, idx: int):
+        if idx >= len(self):
+            raise IndexError()
         return self.data["x"][idx], self.data["y"][idx], self.data["y_hat_old"][idx], self.data["idx"][idx]
 
     @staticmethod
@@ -231,7 +234,9 @@ class BelleIIDistDataset(BelleIIDataset):
         # does not require further shuffeling by the dataloader
         return False
 
-    def __getitem__(self, _):
+    def __getitem__(self, idx):
+        if idx >= len(self):
+            raise IndexError()
         # sample a bucket
         bucket = np.random.choice(self.bucket_idx, p=self.probs)
 

neuro_trigger/pytorch/dataset_filters.py
@@ -14,7 +14,7 @@ import torch
 # helper functions
 def index2mask_array(index_array, n):
     mask_array = torch.zeros(n, dtype=int)
-    mask_array[index_array] = 1
+    mask_array[index_array.long()] = 1
     return mask_array.bool()
 
 
@@ -58,7 +58,26 @@ class ExpertFilter(Filter):
 
 class Max2EventsFilter(Filter):
     def fltr(self, data):
-        return data["ntracks"] > 2
+        # return data["ntracks"] <= 2
+        # create map event -> y -> tracks
+        event_map = {}
+        for idx, (e, y) in enumerate(zip(data["event"], data["y"])):
+            ey = f"{e}"
+            k = f"{y[0]},{y[1]}"
+            if ey not in event_map:
+                event_map[ey] = {}
+            if k not in event_map[ey]:
+                event_map[ey][k] = []
+            event_map[ey][k].append(idx)
+        keep_idx = []
+        for event, value in event_map.items():
+            if len(value) <=2:
+                for key, value in event_map[event].items():
+                    keep_idx += value
+                # keep_events.append(event)
+
+        keep_idx = torch.Tensor(keep_idx)
+        return index2mask_array(keep_idx, len(data['x']))
 
 class DuplicateEventsFilter(Filter):
     def fltr(self, data):
@@ -74,4 +93,4 @@ class DuplicateEventsFilter(Filter):
             keep_idx.append(value[0])
         keep_idx = torch.Tensor(keep_idx)
 
-        return index2mask_array(keep_idx, len(data))
+        return index2mask_array(keep_idx, len(data['x']))

neuro_trigger/tests/test.py
@@ -1,11 +1,14 @@
+from functools import partial
 import logging
 import unittest
 import sys
+import numpy as np
 
 import torch
 from neuro_trigger import main
-from neuro_trigger.pytorch.dataset import BelleIIDataManager
+from neuro_trigger.pytorch.dataset import BelleIIDataManager, BelleIIDistDataset
 from neuro_trigger.pytorch.dataset_filters import ConCatFilter, DuplicateEventsFilter, IdenityFilter, Max2EventsFilter, index2mask_array
+from scipy.stats import norm, uniform
 sys.path.append("/mnt/scratch/juelg/neuro-trigger-v2")
 
 from neuro_trigger.main import DATA_DEBUG, create_trainer_pl_module, prepare_vars
@@ -108,6 +111,52 @@ class FilterTest(unittest.TestCase):
         le = 15
         self.assertEqual(le, len(d))
 
+
+class WeightedSamplerTest(unittest.TestCase):
+    TEST_DATA = "neuro_trigger/tests/test_data_filter.csv"
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.dm = BelleIIDataManager(self.TEST_DATA, logging.getLogger(), dataclass=partial(BelleIIDistDataset,
+    #             dist=norm(loc=-1, scale=1), n_buckets=11))
+    #     return norm(loc=conf_key["norm"]["mean"], scale=conf_key["norm"]["std"])
+    # elif "uniform" in conf_key:
+    #     return uniform(loc=conf_key["uniform"]["lower"], scale=conf_key["uniform"]["upper"])
+
+    def test_distuniform(self):
+        dist = uniform(loc=-1, scale=2)
+        n_buckets=4
+        dm = BelleIIDataManager(self.TEST_DATA, logging.getLogger())
+        d = dm.dataset(dataset_class=partial(BelleIIDistDataset,
+                dist=dist, n_buckets=n_buckets))
+        z = [i[1][0].item() for i in d]
+        hist, bin_edges = np.histogram(z, n_buckets, range=(-1, 1))
+        hist = hist / np.sum(hist)
+
+        # compare to expected values
+        for idx, h in enumerate(hist):
+            # p = dist.cdf(bin_edges[idx+1]) - dist.cdf(bin_edges[idx])
+            # p = d.get_bounds(idx)
+            p = dist.cdf(d.get_bounds(idx)[1]) - dist.cdf(d.get_bounds(idx)[0])
+            self.assertTrue(abs(p - h) < 0.1)
+            # TODO: check if the they are actually from the same bucket
+
+    def test_distnorm(self):
+        dist = norm(loc=0, scale=0.6)
+        n_buckets=4
+        # np.random.seed(1234)
+        dm = BelleIIDataManager(self.TEST_DATA, logging.getLogger())
+        d = dm.dataset(dataset_class=partial(BelleIIDistDataset,
+                dist=dist, n_buckets=n_buckets))
+        z = [i[1][0].item() for i in d]
+        hist, bin_edges = np.histogram(z, n_buckets, range=(-1, 1))
+        hist = hist / np.sum(hist)
+
+        # compare to expected values
+        for idx, h in enumerate(hist):
+            # p = dist.cdf(bin_edges[idx+1]) - dist.cdf(bin_edges[idx])
+            p = dist.cdf(d.get_bounds(idx)[1]) - dist.cdf(d.get_bounds(idx)[0])
+            self.assertTrue(abs(p - h) < 0.1)
 
 
 if __name__ == '__main__':